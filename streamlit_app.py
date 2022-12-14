0cc2b777bb78d43a102f5def445cf6e696b6e25f
snowlakebadge <120558645+snowlakebadge@users.noreply.github.com>
2022-12-14 10:32:17 +0100

    Update streamlit_app.py
    changed header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favorities')
+streamlit.text(':cat: Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
